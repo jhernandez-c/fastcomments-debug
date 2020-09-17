de70cca6fa7285c448109facd7ca73a22f6497b9
winrid <winrid@gmail.com>
2020-09-16 21:29:51 -0700

    functional version that fetches instance and passes it to the vue component

--- chrome/src/content/index.ts
+++ chrome/src/content/index.ts
@@ -1,30 +1,37 @@
 import {Inspector} from '../../../common/inspector';
 
 console.log('Hey, the content script is running!');
+let instances = null;
 
 function codeToInject() {
-// @ts-ignore
-    const originalFastCommentsUI = window.FastCommentsUI;
-// @ts-ignore
-    window.FastCommentsUI = function (element, config) {
-        console.log('PASSTHROUGH', element, config);
-        return originalFastCommentsUI(element, config);
-    }
+    setInterval(() => {
+        console.log('???', window.fcUIInstances);
+        window.parent.postMessage(JSON.stringify({
+            type: 'redirect',
+            fcUIInstances: window.fcUIInstances
+        }), '*');
+    }, 1000);
 }
 
 function embed(fn: Function) {
     const script = document.createElement("script");
     script.text = `(${fn.toString()})();`;
-    document.appendChild(script);
+    document.documentElement.appendChild(script);
 }
 
 embed(codeToInject);
+
+// Listen for any messages
+window.addEventListener("message", (evt) => {
+    // TODO validate request
+    instances = evt.data;
+})
 
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
         switch (message.action) {
             case 'inspect':
-                sendResponse(Inspector.inspectDOM());
+                sendResponse(instances);
                 break;
         }
     }
