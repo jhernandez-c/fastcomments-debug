5cd81ee114efc13c7a551c74e41744d2e8f84384
winrid <winrid@gmail.com>
2020-09-16 17:16:43 -0700

    Initial skeleton

---
+++ chrome/src/devtools/app/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Container } from './styles';
+
+const App: FC = () => (
+  <Container>Hey, it is the DevTools app running on React.js</Container>
+);
+
+export default App;
